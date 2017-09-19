daad2928ba882817543ccd0bcba562013d02b10f
wilshdog <31513793+wilshdog@users.noreply.github.com>
2017-09-19 08:50:25 -0600

    I modified the monster using if/else statements

--- src/monster/holder/MonsterController.java
+++ src/monster/holder/MonsterController.java
@@ -27,9 +27,50 @@ public class MonsterController
 		System.out.println("How many do you want to eat?");
 		Scanner myScanner = new Scanner(System.in);
 		int consumed = myScanner.nextInt();
-		currentMonster.setArmCount(currentMonster.getArmCount()- consumed);
-		System.out.println("Ok, now " + currentMonster.getName() + "has" + currentMonster.getArmCount() + "arms left.");
+		
+		if(consumed < 0)
+		{
+			System.out.println("You cannot eat a negative amount silly human.");
+			consumed = 0;
+		}
+		else if(consumed == 0)
+		{
+			System.out.println("Not that hungry are you?");
+		}	
+		else if(consumed > currentMonster.getArmCount())
+		{
+			System.out.println("That is impossible - I only have " + currentMonster.getArmCount() + " arms!!!");
+		}
+		else
+		{
+			currentMonster.setArmCount(currentMonster.getArmCount() - consumed);
+			System.out.println("Thank you so much! I only have this many arms now: " + currentMonster.getArmCount());
+		}	
+		
+		Scanner eyeScanner = new Scanner(System.in);
+		int consumedEyes = eyeScanner.nextInt();
+		
+		if(consumedEyes < 0)
+		{
+			System.out.println("You cannot eat more than i have you fool!");
+			consumed = 0;
+		}
+		else if(consumedEyes == 0)
+		{
+			System.out.println("Fine.. I didn't want to be eaten anyway :'(");
+		}
+		else if(consumedEyes > currentMonster.getEyeCount())
+		{
+			System.out.println("Boi you really trying to do that I only have " + currentMonster.getArmCount() + " eyes!");
+		}
+		else
+		{
+			currentMonster.setEyeCount(currentMonster.getArmCount() - consumedEyes);
+			System.out.println(" Dude you the best i love that you ate my eyes now i have " + currentMonster.getArmCount() + " left.");
+		}
+	}
 		
 		
 	}
-}
+
+
